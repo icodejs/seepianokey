f37cec5d76d1f335439bdfd98260b2f0b2a0dc3c
Tahir J <icodejs@gmail.com>
2019-03-20 09:53:17 +0000

    WIP chord lesson

--- src/lessons/c-major.js
+++ src/lessons/c-major.js
@@ -3,6 +3,52 @@ export default () => {
 
 };
 
+/*
+function chordLesson() {
+  const chordsInKey = [['C', 'E', 'G'], ['G', 'A', 'C']];
+  const progress = [];
+
+  return notePressed => {
+    const index = progress.length;
+
+    let chord = progress[index];
+
+    if (!chord) {
+      chord = [notePressed];
+    }
+
+    if (chord.length < 3) {
+      chord = [...chord, notePressed];
+    }
+
+    if (chord.length === 3) {
+      const notes = chord;
+      const correct = chordsInKey[index].every(
+        (note, noteIndex) => note === notes[noteIndex],
+      );
+
+      if (!correct) {
+        chord = [];
+        return {};
+      }
+
+      chord = [...chord, notePressed];
+
+      const complete = chordsInKey.length === progress.length;
+
+      return {
+        chordsInKey,
+        correct,
+        progress,
+        complete,
+      };
+    }
+
+    return {};
+  };
+}
+*/
+
 /* function scaleLesson() {
   const scaleBase = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
   const scaleForwards = [...scaleBase, scaleBase[0]];
